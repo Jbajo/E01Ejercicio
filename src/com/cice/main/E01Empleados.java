2d7c241ab14ae8099cc2197be2c1f9fc0e3bab2c
jbajo <javier.bajochacon@gmail.com>
2018-01-23 21:43:03 +0100

    Commit Modificado

--- src/com/cice/main/E01Empleados.java
+++ src/com/cice/main/E01Empleados.java
@@ -4,8 +4,8 @@ import com.cice.negocio.Empresa;
 
 public class E01Empleados {
     public static void main(String[] args) {
-	// write your code here
-        Empresa empresa = new Empresa();
+
+       Empresa empresa = new Empresa();
         empresa.showMenu();
     }
 }
